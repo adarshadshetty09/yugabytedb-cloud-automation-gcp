6463b852495cd998598794260eb2c142858ef9c6
adarshadshetty09 <adarshadshetty09@gmail.com>
2026-02-06 20:53:43 +0530

    yba

---
+++ terraform/Jenkins_VM/modules/gcp_vm/main.tf
@@ -0,0 +1,50 @@
+resource "google_compute_instance" "default" {
+  name         = var.instance_name
+  machine_type = var.machine_type_name
+  zone         = var.zone_name
+
+#   allow_stopping_for_update = var.allow_stopping_for_update_value
+
+  tags = var.netwoork_tags
+  labels = var.instance_labels
+
+  boot_disk {
+    auto_delete = true
+
+  initialize_params {
+    image  = var.instance_image_self_link
+    labels = var.instance_disk_labels
+    size = var.boot_disk_size
+    type = var.boot_disk_type
+  }
+}
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+deletion_protection = var.deletion_protection
+
+allow_stopping_for_update = var.allow_stopping_for_update_value
+
+network_interface {
+  network    = var.network_name
+  subnetwork = var.subnet_work_name
+
+  access_config {}
+}
+
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+service_account {
+  email  = "296131585508-compute@developer.gserviceaccount.com"
+  scopes = ["cloud-platform"]
+}
+
+}
